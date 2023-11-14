fda113082f0926c7f430741281d3cceed80ac4f5
jmerki <70237077+jmerki@users.noreply.github.com>
2023-11-13 19:46:33 -0700

    Finalized Event Card
    Completed white-text layout for Cards for bowling events, made to easily display event name, location, category, date and surface-level statistics.

--- app/src/main/java/com/example/strats/model/BowlingEvent.kt
+++ app/src/main/java/com/example/strats/model/BowlingEvent.kt
@@ -1,4 +1,10 @@
 package com.example.strats.model
 
 class BowlingEvent (val title: String, val date: String, val location: String, val category: EventCategory) {
+
+    val games = mutableListOf<String>()
+
+    fun addGame(game: String) {
+        games.add(game)
+    }
 }
